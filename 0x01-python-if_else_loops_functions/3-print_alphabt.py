4dac9d34846739b4657021e243af833c3f13a99f
samnsi <royallkiing@gmail.com>
2023-03-07 10:26:56 -0800

    Print alphabet without e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
-    print("{:c}".format(alpha_letters), end="")
+      if ord(alpha_letters) == 101  or ord(alpha_letters) == 113:
+         continue
+      print("{:c}".format(alpha_letters), end="")
