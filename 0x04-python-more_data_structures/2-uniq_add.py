cbb173773d54afec3cffa7bc3e00140074542638
samnsi <royallkiing@gmail.com>
2023-03-15 14:10:42 -0700

    Unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = set(my_list)
-    result = 0
-    for i in new_list:
-        result += i
-    return result 
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
